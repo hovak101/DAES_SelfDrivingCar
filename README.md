d467021189de6a500cd221ec83278af0dd54587e
Alex Hovakimyan <hovak101@gmail.com>
2024-02-28 23:49:12 -0800

    added model definition

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Car stuff that uses camera stuff to evade obstacles swoosh swoosh 👀🚗
 
 Execution Stack:
 1. Record Data (RPI)
-4. Send Data (RPI)
+4. Get Data (PC)
 5. Train Model (PC)
 6. Send Model (PC)
 7. Run Car (RPI)
@@ -17,15 +17,15 @@ environment with the following command:
 
 2. Then install all dependencies with the  following command: 
 
-  ```pip install -r pi_requirements.txt```
+  ```pip install -r requirements/pi_requirements.txt```
 
 ## To replicate this setup on PC(neural net training):
 
 1. navigate to the root directory of this project and create a virtual
 environment with the following command: 
 
-  ```python -m venv train_env```
+  ```conda create tf```
 
 2. Then install all dependencies with the  following command: 
 
-  ```pip install -r train_requirements.txt```
+  ```pip install -r requirements\train_requirements.txt```
--- src/train_model.py
+++ src/train_model.py
@@ -14,8 +14,9 @@ import os
 from dotenv import load_dotenv
 import numpy as np
 import tensorflow as tf 
-import keras
-from keras import layers
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization, Input, GlobalAveragePooling2D
+from keras.callbacks import EarlyStopping
 
 load_dotenv()
 
@@ -39,3 +40,42 @@ for img_name in os.listdir(frames_dir):
     
 # convert list of numpy arrays to numpy array
 frames = np.array(frames)
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  try:
+    # Currently, memory growth needs to be the same across GPUs
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
+  except RuntimeError as e:
+    # Memory growth must be set before GPUs have been initialized
+    print(e)
+
+# Define model
+model = Sequential([
+    Input(shape=(480, 640, 3)),
+    Conv2D(32, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+    BatchNormalization(),
+    
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+    BatchNormalization(),
+    
+    Conv2D(128, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+    BatchNormalization(),
+    
+    GlobalAveragePooling2D(),
+    Dense(512, activation='relu'),
+    Dropout(0.5),
+    Dense(5, activation='sigmoid')
+])
+
+model.compile(optimizer='adam', loss='mse')
+
+early_stopping = EarlyStopping(monitor='val_loss', patience=5, verbose=1, 
+                               mode='min', restore_best_weights=True)
+
+model.fit(frames, act_vals, epochs=100, batch_size=8, validation_split=0.2, 
+                                callbacks=[early_stopping])
