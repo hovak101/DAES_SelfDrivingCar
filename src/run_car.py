5368b1c7a2d45164858742bf6cf8b2aa925b9855
alex <hovak101@gmail.com>
2024-03-04 22:54:57 -0800

    entire software pipeline complete

--- src/run_car.py
+++ src/run_car.py
@@ -12,6 +12,7 @@ import tensorflow as tf
 from dotenv import load_dotenv
 import os
 import cv2
+from rcDriverController import RcDriver
 
 load_dotenv()
 
@@ -25,13 +26,15 @@ interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
-# Test the model on random input data
+# check shapes 
 input_shape = input_details[0]['shape']
-print(input_shape)
+print("input shape:", input_shape)
 output_shape = output_details[0]['shape']
-print(output_shape)
+print("output shape:", output_shape)
 
 vid = cv2.VideoCapture(0)
+
+rc_driver = RcDriver()
 
 while True:
     try:
@@ -48,11 +51,20 @@ while True:
         back_left = output_data[0][1]
         front_right = output_data[0][2]
         back_right = output_data[0][3]
-        # print("FL", front_left)
+        print("FL:", front_left)
         # print("BL", back_left)
-        # print("FR", front_right)
+        print("FR:", front_right)
         # print("BR", back_right)
+        reverse_left = False
+        reverse_right = False
         
+        if front_left < 0:
+            reverse_left = True
+        
+        if front_right < 0: 
+            reverse_right = True
+            
+        rc_driver.set_pins(abs(front_left), abs(front_right), reverse_left, reverse_right)
         
     except KeyboardInterrupt:
         vid.release()
